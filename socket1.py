eeae3e1aaacdfff134b9bb50bb3b94dc58e94f8a
Kristina Palmer <Kristina.Palmer@vu.com>
2022-04-30 13:09:21 -0500

    1st Attempt

--- socket1.py
+++ socket1.py
@@ -1,16 +1,38 @@
+from base64 import decode
 import socket
 
 def getWebData():
     mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     mysock.connect(('data.pr4e.org', 80))
-    cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
+    cmd = 'GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n'.encode()
     mysock.send(cmd)
-
+    
+    header_lists = list()
+    header_dictionary = dict()
+       
     while True:
         data = mysock.recv(512)
         if len(data) < 1:
             break
-        print(data.decode(),end='')
+        decode_data = data.decode()
+        #mysock.close()
 
-    mysock.close()
-##getWebData()
+    def splitLines(): 
+        data = decode_data.replace("\r",' ')
+        data = data.splitlines()
+        #return data
+
+    for line in data: 
+        if line == ' ': 
+            break
+        else: 
+            header_lists.append(line)
+    for item in header_lists: 
+        item = item.split(": ")
+        header_dictionary[item[0]] = item[1]
+    
+    for key,value in header_dictionary.items(): 
+        print(key,value)
+
+    splitLines()
+getWebData()
