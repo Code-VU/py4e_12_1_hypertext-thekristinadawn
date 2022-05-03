fdb77bce187d5c344ff1e35880aa882152cfff10
Kristina Palmer <Kristina.Palmer@vu.com>
2022-05-02 21:41:03 -0500

    2nd attempt

--- socket1.py
+++ socket1.py
@@ -6,33 +6,33 @@ def getWebData():
     mysock.connect(('data.pr4e.org', 80))
     cmd = 'GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n'.encode()
     mysock.send(cmd)
-    
-    header_lists = list()
-    header_dictionary = dict()
        
     while True:
         data = mysock.recv(512)
         if len(data) < 1:
             break
         decode_data = data.decode()
-        #mysock.close()
+    
+    header_lists = list()
+    header_dictionary = dict()
 
     def splitLines(): 
         data = decode_data.replace("\r",' ')
-        data = data.splitlines()
+        data = decode_data.splitlines()
         #return data
 
-    for line in data: 
-        if line == ' ': 
-            break
-        else: 
-            header_lists.append(line)
-    for item in header_lists: 
-        item = item.split(": ")
-        header_dictionary[item[0]] = item[1]
+        for line in data: 
+            if line == ' ': 
+                break
+            else: 
+                header_lists.append(line)
+        for item in header_lists: 
+            item = item.split()
+        print(item)
     
-    for key,value in header_dictionary.items(): 
-        print(key,value)
+        for key,value in header_dictionary.items(): 
+            print(key,value)
 
+    mysock.close()
     splitLines()
 getWebData()
